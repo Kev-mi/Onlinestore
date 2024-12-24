4d2db62bc5ab2b7f133e6f0b2c25f8badebffc05
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2024-12-24 02:08:59 +0100

    Update AdminManager.py

--- model/AdminManager.py
+++ model/AdminManager.py
@@ -2,8 +2,7 @@ import view.CustomerMenu as CustomerMenu
 
 class AdminManager:
 
-    def __init__(self, cursor):
-        self.cursor = cursor
+    def __init__(self):
 
         self.product_id_search_input = None
         self.product_name_search_input = None
@@ -11,6 +10,32 @@ class AdminManager:
         self.product_supplier_search_input = None
         self.product_supplier_search_input = None
         self.product_code_input_2 = None
+
+        self.id_discount_input = None
+        self.name_discount_input = None
+        self.discount_input = None
+        self.lower_date_input = None
+        self.upper_date_input = None
+        self.product_code_input = None
+        self.discount_id_2 = None
+
+        self.name_input = None
+        self.street_input = None
+        self.zip_code_input = None
+        self.city_input = None
+        self.country_input = None
+        self.phone_number_input = None
+
+        self.id_input_product = None
+        self.product_name_input = None
+        self.product_base_price_input = None
+        self.product_supplier_input = None
+        self.product_quantity_input = None
+        self.product_id_input_2 = None
+        self.product_quantity_input_2 = None
+
+        self.selected_product_id = None
+        self.selected_discount_id = None
 
         self._current_date = None
 
