5d2096a1cd311a7cd5a187482fb8035fa9c013f2
Kev-mi <90063807+Kev-mi@users.noreply.github.com>
2025-01-12 21:51:24 +0100

    Update DatabaseManager.py

--- model/DatabaseManager.py
+++ model/DatabaseManager.py
@@ -1,11 +1,31 @@
 import sqlite3
 
 
-#class to manage connection
+#class to manage db
 class DatabaseManager:
     def __init__(self, db_name):
         self.conn = sqlite3.connect(db_name)
         self.cursor = self.conn.cursor()
+        self.begin_transaction()
+
+    def begin_transaction(self):
+        try:
+            if not self.conn.in_transaction:
+                self.cursor.execute("BEGIN")
+        except sqlite3.Error as e:
+            print(f"Error: {e}")
+
+    def commit_transaction(self):
+        try:
+            self.conn.commit()
+        except sqlite3.Error as e:
+            print(f"Error: {e}")
+
+    def rollback_transaction(self):
+        try:
+            self.conn.rollback()
+        except sqlite3.Error as e:
+            print(f"Error: {e}")
 
     def execute_query(self, query, params=()):
         try:
